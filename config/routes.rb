bd9ca64c766b62c7702a2d51f226f1199af9de9b
phonga3cvp <phonga3cvp@gmail.com>
2016-07-26 10:58:14 +0700

    Categories-show

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,10 @@ Rails.application.routes.draw do
   get "help" => "static_pages#help"
   get "signup" => "users#new"
   post "signup" => "users#create"
-  resources :users
   get    "login" => "sessions#new"
   post   "login" => "sessions#create"
   delete "logout" => "sessions#destroy"
+
+  resources :users
+  resources :categories, only: [:index, :show]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,3 +11,15 @@ User.create!(name:  "Example User",
     password:              password,
     password_confirmation: password,admin: 0)
 end
+Category.create!(name: "Basic 500")
+Category.create!(name: "At a Restaurant")
+Category.create!(name: "On a trip")
+500.times do 
+  Word.create!(content: "English", mean: "Vietnamese", category_id: 1)
+end
+100.times do 
+  Word.create!(content: "Restaurant", mean: "Nha hang", category_id: 2)
+end
+100.times do
+  Word.create!(content: "Trip", mean: "Chuyen di", category_id: 3)
+end
